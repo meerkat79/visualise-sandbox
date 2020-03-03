0628d7526be1eddb6444ba1a7c0b95462d0ccbea
Rudi van Litsenborgh <rvlglobe@gmail.com>
2020-03-03 11:41:53 +0000

    update mock data for use

--- src/views/chart-1/chart1.js
+++ src/views/chart-1/chart1.js
@@ -1,23 +1,78 @@
-import React       from 'react';
+import React from 'react';
+import * as d3 from 'd3';
 
-export default class Chart1 extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = { data: {} };
+const nameData = './data/nameData.json';
+const scopeData = './data/scopeData.json';
+
+export default class Chart1 extends React.Component {
+
+  constructor(props){
+    super(props)
+  
+    this.state = {
+      initialBooks:[
+        {
+            name: "Harry Potter and the Philosophers Stone",
+            author: "J. K. Rowling",
+            genre: "fantasy"
+        },{
+            name: "The Pedagogy of Freedom",
+            author: "Bell hooks",
+            genre: "non-fiction"
+        },{
+            name: "Harry Potter and the Chamber of Secrets",
+            author: "J. K. Rowling",
+            genre: "fantasy"
+        },{
+            name: "Gilgamesh",
+            author: "Derrek Hines",
+            genre: "poetry"
+        }
+    ],
+    nameData: [],
+    scopeData: []
+    }
   }
 
-  randomizeData() {
-    // temp
+  getData = () => {
+    // first get name data
+    fetch(nameData)
+      .then(res => {
+        return res.json();
+      }).then(data => {        
+        this.setState({nameData: data.nameData});
+      });
+
+    // next get scope data
+    fetch(scopeData)
+      .then( res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({scopeData: data.scopeData});
+        console.log(this.state);
+      })
   }
 
-  render() {
-    return <div>
-      <h1>Playing With React and D3</h1>
-      <div className="controls">
-        <button className="btn randomize" onClick={() => this.randomizeData()}>
-          Randomize Data
-        </button>
+
+  componentDidMount() {
+
+    d3.selectAll("p").style("color", "blue");
+    d3.select(this.refs.test).style("background-color", "blue");
+
+    this.getData();
+
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Playing With React and D3</h1>
+        <p>test</p>
+        <div ref="test">foo bar</div>
       </div>
-    </div>
+    )
   }
+
+
 }
